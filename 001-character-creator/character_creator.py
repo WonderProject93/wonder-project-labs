078b51850380f8be9f15eb6fe972a96250b810a2
WonderProject93 <thewonderproject93@gmail.com>
2026-09-09 20:22:09 +0000

    Add first Wonder Project Lab: Character Creation

---
+++ 001-character-creator/character_creator.py
@@ -0,0 +1,10 @@
+intro = "STORY ENGINE\nCharacter Creation"
+print(intro + "\n")
+character_name = input("What is your characer's name?\n")
+character_home = input("Where is your character from?\n")
+current_weapon = input("What is your character's staring weapon?\n")
+current_goal = input("What is your character's current goal?\n")
+
+character_record = "Name: " + character_name + "\nHome: " + character_home + "\nWeapon: " + current_weapon + "\nGoal: " + current_goal
+print("====================\nCHARACTER RECORD\n====================")
+print(character_record + "\n====================")
