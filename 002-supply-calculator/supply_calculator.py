9fbeb25b538180a44117c0a7ab48b37d80631549
WonderProject93 <thewonderproject93@gmail.com>
2026-09-10 19:38:55 +0000

    Project 002, supply_ calculator.py

---
+++ 002-supply-calculator/supply_calculator.py
@@ -0,0 +1,17 @@
+intro = "STORY ENGINE\nExpedition Supply Calculator"
+print(intro)
+name = (input("What is your character's name? "))
+starting_gold = float(input("How much gold does your character have? "))
+health_potions_want = int(input("How many health potions do they want? "))
+health_potion_cost = float(input("What is the price of each health potion? "))
+expedition_timeline = int(input("How many days will the expedition last? "))
+daily_food_cost = float(input("What is the daily food cost? "))
+
+total_potion_cost = health_potions_want * health_potion_cost
+expedition_food_cost = expedition_timeline * daily_food_cost
+
+expedition_cost = total_potion_cost + expedition_food_cost
+
+remaining_gold = starting_gold - expedition_cost
+
+print(f"========================\nEXPEDITION SUPPLY REPORT\n========================\nCharacter: {name}\nStaring Gold: {starting_gold}\nPotion Cost: {total_potion_cost}\nFood Cost: {expedition_food_cost}\nTotal Cost: {expedition_cost}\nGold Remaining: {remaining_gold}\n========================")
